8b06d006475c8ae71b51af80ebb91a17f4c0c66e
Uliana Dzhumok <uliana.dzhumok@gmail.com>
2020-02-02 21:15:35 +0700

    feat: Page Object + Chain of invocations

---
+++ src/test/java/utils/DriverFactory.java
@@ -0,0 +1,29 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+
+public class DriverFactory {
+    private static WebDriver driver;
+    private static final String DRIVER_PATH = "src/test/resources/";
+
+    public static WebDriver getDriver(Browser browser) {
+        File file;
+
+        switch (browser) {
+            case CHROME:
+                file = new File(DRIVER_PATH + "chromedriver.exe");
+                System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+                driver = new ChromeDriver();
+                break;
+
+            default:
+        }
+
+        if (PropertyReader.getSize().compareTo("true") == 0) driver.manage().window().maximize();
+
+        return driver;
+    }
+}
